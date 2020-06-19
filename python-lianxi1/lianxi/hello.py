e2406b2eeb6a00cdcbfb34f6c6a05d5172e75ae0
zhan <1955422532@qq.com>
2020-06-19 15:44:40 +0800

    提交

---
+++ python-lianxi1/lianxi/hello.py
@@ -0,0 +1,11 @@
+import numpy
+import matplotlib
+import sklearn
+import pandas
+
+print("hello")
+
+a = [2,3,4,2,5,3,2]
+from collections import Counter
+print(Counter(a))
+print(Counter(a).most_common(1))
